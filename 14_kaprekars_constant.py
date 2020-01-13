892e4c2ddcacea4cb90b051cba8f85f950b3be65
fares-ds <faressayahds@gmail.com>
2020-01-13 17:42:51 +0100

    14_kaprekars_constant

--- 14_kaprekars_constant.py
+++ 14_kaprekars_constant.py
@@ -1,13 +1,26 @@
 def kaprekars_constant(num):
-    digit_str = list(str(num))
+    counter = 0
+    while True:
+        str_num = str(num)
+        if len(str_num) < 4:
+            str_num = str_num + '0' * (4 - len(str_num))
 
-    bigger = list(sorted(digit_str, reverse=True))
-    smaller = list(sorted(digit_str))
+        digit_str = list(str_num)
 
-    bigger = int(''.join(bigger))
-    smaller = int(''.join(smaller))
+        bigger = list(sorted(digit_str, reverse=True))
+        smaller = list(sorted(digit_str))
 
-    return bigger - smaller
+        bigger = int(''.join(bigger))
+        smaller = int(''.join(smaller))
+
+        diff = bigger - smaller
+
+        if diff == int(''.join(digit_str)):
+            break
+        num = diff
+        counter += 1
+        print(diff)
+    return counter
 
 
 print(kaprekars_constant(3524))
